c03b620b4909a87b778f9fdf9f133d7313899a21
Isaac <isaacpereztouceda@gmail.com>
2025-07-14 02:55:49 -0400

    Adding sturdiness

--- src/main/controller/RegisterController.java
+++ src/main/controller/RegisterController.java
@@ -9,6 +9,8 @@ public class RegisterController implements ActionListener {
     private Register register;
     private UCVDataReader ucvLector;
     private SCUDataManager scuManager;
+
+    private final int CAMP_EMPTY = -1;
     private final int USER_TAKEN = 0;
     private final int CI_NOT_FOUND = 1;
     private final int ALREADY_REGISTER = 2;
@@ -26,15 +28,21 @@ public class RegisterController implements ActionListener {
 
         String command = e.getActionCommand();
         
-        if (command == "REGISTRAR") {
+        if ("REGISTRAR".equals(command)) {
 
-            String cI = register.getCi();
+            String ci = register.getCi();
             String user = register.getUser();
             String key = register.getKey();
+
+            if (campIsEmpty(ci, user, key)) {
+                reply(CAMP_EMPTY);
+                return; 
+            }
+        
             ucvLector = new UCVDataReader();
             scuManager = new SCUDataManager();
 
-            boolean isCiFind = ucvLector.findCi(cI); 
+            boolean isCiFind = ucvLector.findCi(ci); 
             String userType = ucvLector.getUserType();
             
             if (!isCiFind) {
@@ -43,9 +51,9 @@ public class RegisterController implements ActionListener {
 
             } else {
 
-                int reply = scuManager.invalidOperation(cI, user);
-                if(reply == SUCCESS_OP) {
-                    scuManager.createAccount(cI,user,userType,key);
+                int reply = scuManager.invalidOperation(ci, user);
+                if(reply == SUCCESS_OP) {  
+                    scuManager.createAccount(ci,user,userType,key);
                     reply(SUCCESS_OP);
                 } else {
                     reply(reply);
@@ -59,7 +67,7 @@ public class RegisterController implements ActionListener {
     public void reply(int messageId) {
         switch (messageId) {
             case USER_TAKEN:
-                register.warning("Este usuario ya se encuentra tomado, intente otro username.");
+                register.warning("Este nombre de usuario ya se encuentra ocupado, por favor intente otro.");
                 break;
             case CI_NOT_FOUND:
                 register.warning("Usted no se encuentra en el registro UCV, por favor visite secretaria.");
@@ -67,12 +75,19 @@ public class RegisterController implements ActionListener {
             case ALREADY_REGISTER:
                 register.warning("Usted ya se encuentra registrado en el sistema.");
                 break;
+            case CAMP_EMPTY:
+                register.warning("Por favor, complete todos los campos requeridos.");
+                break;
             case SUCCESS_OP:
                 register.confirm("El registro ha sido exitoso");
                 break;
             default:
                 break;
         }
+    }
+
+    public boolean campIsEmpty(String ci, String user, String key) {
+        return ci.isEmpty() || user.isEmpty() || key.isEmpty();
     }
 
 }
