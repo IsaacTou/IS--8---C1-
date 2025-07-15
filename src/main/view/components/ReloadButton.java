34c774f2d6c698595c7a505c053dd17a26e2f91d
Santiago <150873551+santiago-commit@users.noreply.github.com>
2025-07-15 21:58:33 +0000

    Update ReloadButton.java

--- src/main/view/components/ReloadButton.java
+++ src/main/view/components/ReloadButton.java
@@ -5,12 +5,7 @@ import java.awt.Color;
 public class ReloadButton extends Button {
     public ReloadButton() {
         super("Recargar", new Color(4, 113, 166), 127, 28); //Color azul y tamaño por defecto
-    }
-
-    @Override
-    protected void configurarAccion() {
-        addActionListener(e -> {
-            // Lógica de recargar el monedero
-        });
+		setActionCommand("RECARGAR");
+		setToolTipText("Recargar saldo"); 
     }
 }
