0d8fc60769b4d5097d7d5c966a6b9caf7a1f9237
Santiago <150873551+santiago-commit@users.noreply.github.com>
2025-07-15 14:53:01 +0000

    Add files via upload

---
+++ src/main/controller/PurseController.java
@@ -0,0 +1,53 @@
+package src.main.controller;
+import src.main.view.pages.PurseView;
+import src.main.model.*;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import src.main.utils.*;
+
+public class PurseController implements ActionListener {
+
+	private PurseView purseView;
+	private Navigate navigate;
+
+	private final int USER_TAKEN = 0;
+	private final int CI_NOT_FOUND = 1;
+	private final int ALREADY_REGISTER = 2;
+	private final int SUCCESS_OP = 3;
+
+	public PurseController(PurseView purseView, Navigate navigate) {
+		this.purseView = purseView;
+		this.navigate = navigate;
+		this.purseView.setController((ActionListener) this);
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+
+		String command = e.getActionCommand();
+
+		if ("RECARGAR".equals(command)) {
+			String amount = purseView.getAmount();
+		}
+
+	}
+	public void reply(int messageId) {
+		switch (messageId) {
+			case USER_TAKEN:
+			purseView.warning("Este nombre de usuario ya se encuentra ocupado, por favor intente otro.");
+			break;
+			case CI_NOT_FOUND:
+			purseView.warning("Usted no se encuentra en el registro UCV, por favor visite secretaria.");
+			break;
+			case ALREADY_REGISTER:
+			purseView.warning("Usted ya se encuentra registrado en el sistema.");
+			break;
+			case SUCCESS_OP:
+			purseView.confirm("El registro ha sido exitoso");
+			break;
+			default:
+			break;
+		}
+	}
+
+}
