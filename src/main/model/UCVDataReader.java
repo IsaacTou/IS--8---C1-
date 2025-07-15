8ecef3b538482e69ba21e607f27ae36dbfbc51d4
Santiago <150873551+santiago-commit@users.noreply.github.com>
2025-07-15 14:53:47 +0000

    Add files via upload

---
+++ src/main/model/UCVDataReader.java
@@ -0,0 +1,38 @@
+package src.main.model;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+
+public class UCVDataReader {
+    private String ruteString;
+    private String userType;
+    private static int IDENTIFICATION_ID = 0;
+    private static int UCV_USER_TYPE = 1;
+
+    public UCVDataReader () {
+        ruteString = "src/main/data/UCVDataBase.txt";
+        userType = "";
+    }
+
+    public boolean findCi(String ci) {
+        try (BufferedReader br = new BufferedReader(new FileReader(ruteString))){
+            String line;
+            while ((line = br.readLine()) != null) {
+                String[] parts = line.split(",");
+                if (parts[IDENTIFICATION_ID].equals(ci)) {
+                    this.userType = parts[UCV_USER_TYPE];
+                    return true;
+                }
+            }
+        } catch (IOException e) {
+            System.err.println("Error al leer el archivo: " + e.getMessage());
+        }
+        return false;
+    }
+
+    public String getUserType() {
+        return this.userType;
+    }
+    
+}
