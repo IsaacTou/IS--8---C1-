70452e7477d0cbfa7e813797eacf6ca3aa52262d
Santiago <150873551+santiago-commit@users.noreply.github.com>
2025-07-15 21:49:32 +0000

    Update RegisterButton.java

--- src/main/view/components/RegisterButton.java
+++ src/main/view/components/RegisterButton.java
@@ -1,12 +1,11 @@
 package src.main.view.components;
 import java.awt.Color;
 
-// Botón de Registro
+@SuppressWarnings("serial")
 public class RegisterButton extends Button {
     public RegisterButton() {
         super("Registrar", new Color(4, 113, 166), 127, 28); //Color azul y tamaño por defecto
         setActionCommand("REGISTRAR");
         setToolTipText("Registrar un nuevo usuario"); 
     }
-
 }
