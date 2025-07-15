97e3de9b5d240a3b55742dfed9542f08ef69d034
Santiago <150873551+santiago-commit@users.noreply.github.com>
2025-07-15 21:50:39 +0000

    Update LoginView.java

--- src/main/view/pages/LoginView.java
+++ src/main/view/pages/LoginView.java
@@ -2,76 +2,72 @@ package src.main.view.pages;
 import javax.swing.*;
 import java.awt.event.ActionListener;
 
+@SuppressWarnings("serial")
 public class LoginView extends JFrame {
-    JPanel panel;
-    JTextField userBox;
-    JPasswordField keyBox;
-    JButton button;
+	JPanel panel;
+	JTextField userBox;
+	JPasswordField keyBox;
+	JButton button;
 
+	public LoginView() {
+		setSize(850,650); // (ancho, largo)
+		setDefaultCloseOperation(EXIT_ON_CLOSE); // Cuando cierre la ventana el programa finalizara
+		setTitle("Sistema del comedor universitario SCU - Login"); // titulo de la ventana
+		setLocationRelativeTo(null); // centra la localizacion de la pantalla 
+		addPanel();
+	}
 
-    public LoginView() {
-        setSize(850,650); // (ancho, largo)
-        setDefaultCloseOperation(EXIT_ON_CLOSE); // Cuando cierre la ventana el programa finalizara
-        setTitle("Sistema del comedor universitario SCU - Login"); // titulo de la ventana
-        setLocationRelativeTo(null); // centra la localizacion de la pantalla 
-        addPanel();
+	public void addPanel() {
+		panel = new JPanel();
+		panel.setLayout(null);
+		this.getContentPane().add(panel);
+		addUserBox();
+		addKeyBox();
+		addButton();
+	}
 
-    }
+	public void addUserBox() {
+		userBox = new JTextField();
+		userBox.setBounds(10,20,200,50);
+		panel.add(userBox);
+	}
 
-    public void addPanel() {
-        panel = new JPanel();
-        panel.setLayout(null);
-        this.getContentPane().add(panel);
-        addUserBox();
-        addKeyBox();
-        addButton();
+	public void addKeyBox () {
+		keyBox = new JPasswordField();
+		keyBox.setBounds(10,100,200,50);
+		panel.add(keyBox);
+	}
 
-    }
+	public void addButton () {
+		button = new JButton("Iniciar sesion");
+		button.setBounds(10,200,200,50);
+		button.setActionCommand("LOGUEAR");
+		panel.add(button);
+	}
 
-    public void addUserBox() {
-        userBox = new JTextField();
-        userBox.setBounds(10,20,200,50);
-        panel.add(userBox);
-    }
+	public String getUser() {
+		return userBox.getText();
+	}
 
-    public void addKeyBox () {
-        keyBox = new JPasswordField();
-        keyBox.setBounds(10,100,200,50);
-        panel.add(keyBox);
-    }
+	public String getKey() {
+		return new String(keyBox.getPassword());
+	}
 
-    public void addButton () {
-        button = new JButton("Iniciar sesion");
-        button.setBounds(10,200,200,50);
-        button.setActionCommand("LOGUEAR");
-        panel.add(button);
-    }
+	public void setController(ActionListener controller) {
+		button.addActionListener(controller);
+	}
 
-    public String getUser() {
-        return userBox.getText();
-    }
+	public void confirm(String message) {
+		JOptionPane.showMessageDialog(null,
+			message,
+			"Operacion exitosa",
+			JOptionPane.DEFAULT_OPTION);
+	}
 
-    public String getKey() {
-        return new String(keyBox.getPassword());
-
-    }
-
-    public void setController(ActionListener controller) {
-        button.addActionListener(controller);
-    }
-
-    public void confirm(String message) {
-        JOptionPane.showMessageDialog(null,
-                                      message,
-                                      "Operacion exitosa",
-                                      JOptionPane.DEFAULT_OPTION);
-    }
-    
-    public void warning(String message) {
-        JOptionPane.showMessageDialog(null,
-                                      message,
-                                      "Operacion fallida",
-                                      JOptionPane.WARNING_MESSAGE);
-    }
-    
+	public void warning(String message) {
+		JOptionPane.showMessageDialog(null,
+			message,
+			"Operacion fallida",
+			JOptionPane.WARNING_MESSAGE);
+	}
 }
