c6bd227b66dd6e57630e0b2acbe4c00816dfee17
adrián jaén <adrianjaensiu@gmail.com>
2025-07-16 18:04:58 -0400

    CalculateCCButton.java

--- src/main/view/components/CalculateCCButton.java
+++ src/main/view/components/CalculateCCButton.java
@@ -6,6 +6,6 @@ public class CalculateCCBButton extends Button {
     public CalculateCCBButton() {
         super("Calcular CCB", new Color(4, 113, 166), 127, 28);
         setActionCommand("CALCULAR_CCB");
-        setToolTipText("Calcular costo de comida y bebida");
+        setToolTipText("Calcular costo");
     }
 }
