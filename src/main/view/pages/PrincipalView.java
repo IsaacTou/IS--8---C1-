a07125052ffe029810b2366cbedfc9a1e26a1360
Santiago <150873551+santiago-commit@users.noreply.github.com>
2025-07-16 23:26:18 +0000

    Update PrincipalView.java

--- src/main/view/pages/PrincipalView.java
+++ src/main/view/pages/PrincipalView.java
@@ -1,22 +1,19 @@
 package src.main.view.pages;
-    
 import java.awt.Font;
 import javax.swing.*;
-import src.main.model.SesionUser;
-import src.main.utils.Navigate;
 import src.main.view.components.*;
 import java.awt.event.ActionListener;
 
 @SuppressWarnings("serial")
 public class PrincipalView extends JFrame {
-    
+
 	JPanel panel;
-    JPanel centerPanel;
+	JPanel centerPanel;
 	ImagePanel imageBackground;
-    ImagePanel centerBackground;
-    ConsumeButton consume;
-    CheckMenuButton checkMenu;
-    CheckMenuButton checkPurse;
+	ImagePanel centerBackground;
+	ConsumeButton consume;
+	CheckMenuButton checkMenu;
+	CheckPurseButton checkPurse;
 
 	int boxXPosition = 520;
 	int ciBoxYPosition = 230;
@@ -24,78 +21,73 @@ public class PrincipalView extends JFrame {
 	int keyBoxYPosition = 330;
 	int xPosition = 440;
 
-
 	public PrincipalView() {
-    setSize(850,650);
-    setDefaultCloseOperation(EXIT_ON_CLOSE);
-    setTitle("SGCU"); 
-    setLocationRelativeTo(null);
-    addPanel();
-    addCenterPanel();
-    addButtons();
-    addImage();
-
-    // Verificar si es admin y mostrar botones adicionales
-    if(SesionUser.getInstance().getUser() != null && 
-       SesionUser.getInstance().getUser().getUserType().equals("admin")) {
-        addAdminButtons();
-    }
-}
+		setSize(850,650); // (ancho, largo)
+		setDefaultCloseOperation(EXIT_ON_CLOSE); // Cuando cierre la ventana el programa finalizara
+		setTitle("SGCU"); // titulo de la ventana
+		setLocationRelativeTo(null); // centra la localizacion de la pantalla 
+		setLayout(null);
+		addPanel();
+		addCenterPanel();
+		addButtons();
+		addImage();
+	}
 
 	private void addPanel() {
-        panel = new JPanel();
-        panel.setLayout(null);
-        panel.setBounds(0, 0, 220, 650); // Panel izquierdo
-        this.getContentPane().add(panel);
-        JLabel title = new JLabel("APARTADOS:");
-	    title.setBounds(0, 0, 220, 350);
-	    title.setHorizontalAlignment(SwingConstants.CENTER);
-	    title.setFont(new Font("Sans Serif", Font.BOLD, 20));
-        panel.add(title);
-    }
-    
-    private void addImage() {
-        imageBackground = new ImagePanel("principal_background.png",630, 650, 230, 0);
-        this.getContentPane().add(imageBackground);
-    }
+		panel = new JPanel();
+		panel.setLayout(null);
+		panel.setBounds(0, 0, 220, 650); // Panel izquierdo
+		this.getContentPane().add(panel);
+		JLabel title = new JLabel("APARTADOS:");
+		title.setBounds(0, 0, 220, 350);
+		title.setHorizontalAlignment(SwingConstants.CENTER);
+		title.setFont(new Font("Sans Serif", Font.BOLD, 20));
+		panel.add(title);
+	}
 
-    private void addCenterPanel() {
-        centerPanel = new JPanel();
-        centerPanel.setLayout(null);
-        centerPanel.setBounds(450, 200, 200, 250); // Posición y tamaño del panel central
+	private void addImage() {
+		imageBackground = new ImagePanel("principal_background.png",630, 650, 230, 0);
+		this.getContentPane().add(imageBackground);
+	}
 
-        // Imagen pequeña dentro del panel central
-        centerBackground = new ImagePanel("user.png",100,100,50,50);
-        centerPanel.add(centerBackground);
+	private void addCenterPanel() {
+		centerPanel = new JPanel();
+		centerPanel.setLayout(null);
+		centerPanel.setBounds(450, 200, 200, 250); // Posición y tamaño del panel central
 
-        this.getContentPane().add(centerPanel);
-    }
+		// Imagen pequeña dentro del panel central
+		centerBackground = new ImagePanel("user.png",100,100,50,50);
+		centerPanel.add(centerBackground);
 
-    private void addButtons() {
-        consume = new ConsumeButton();
-        consume.setBounds(40, 160, 120, 50);
-        consume.setFont(new Font("Arial", Font.BOLD, 14));
-        centerPanel.add(consume);
+		this.getContentPane().add(centerPanel);
+	}
 
-        checkMenu = new CheckMenuButton();
-        checkMenu.setBounds(18,300,190,50);
-        panel.add(checkMenu);
-    }
+	private void addButtons() {
+		JLabel balanceLabel = new JLabel("Saldo actual: 0.0 Bs");
+		balanceLabel.setBounds(0, 0, 220, 50);
+		balanceLabel.setFont(new Font("Sans Serif", Font.BOLD, 20));
+		balanceLabel.setHorizontalAlignment(SwingConstants.CENTER);
+		panel.add(balanceLabel);
 
-       public void setController(ActionListener controller) {
-               checkPurse.addActionListener(controller);
-       }
+		checkPurse = new CheckPurseButton();
+		checkPurse.setBounds(18, 50, 190, 50);
+		panel.add(checkPurse);
 
+		consume = new ConsumeButton();
+		consume.setBounds(40, 160, 120, 50);
+		consume.setFont(new Font("Arial", Font.BOLD, 14));
+		centerPanel.add(consume);
 
-    private void addAdminButtons() {
-    CalculateCCBButton calculateCCBButton = new CalculateCCBButton();
-    calculateCCBButton.setBounds(18, 350, 190, 50);
-    calculateCCBButton.addActionListener(e -> {
-        this.dispose();
-        Navigate.getInstance().initCCBView();
-    });
-    panel.add(calculateCCBButton);
+		checkMenu = new CheckMenuButton();
+		checkMenu.setBounds(18,300,190,50);
+		panel.add(checkMenu);
+	}
+
+	public void setController(ActionListener controller) {
+		checkPurse.addActionListener(controller);
+	}
+
+	public void addAdminButtons() {
+
+	}
 }
-
-}
-
