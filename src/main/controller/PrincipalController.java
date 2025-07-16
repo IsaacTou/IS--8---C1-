233e8ea796193281a63dfd1dd56d44758979c03a
Santiago <150873551+santiago-commit@users.noreply.github.com>
2025-07-16 23:22:42 +0000

    Add files via upload

---
+++ src/main/controller/PrincipalController.java
@@ -0,0 +1,31 @@
+package src.main.controller;
+import src.main.view.pages.*;
+import src.main.model.*;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import src.main.utils.*;
+
+public class PrincipalController implements ActionListener {
+
+	private PrincipalView principalView;
+
+	public PrincipalController(PrincipalView principalView) {
+		this.principalView = principalView;
+		this.principalView.setController((ActionListener) this);
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+
+		String command = e.getActionCommand();
+		System.out.println("ESTA FUNCION NO SE EJECUTA NUNCA CARAJO");
+
+		if ("OPEN_WALLET".equals(command)) {
+			Navigate.getInstance().initPurse();
+		}
+	}
+
+	public void reply(int messageId) {
+
+	}
+}
