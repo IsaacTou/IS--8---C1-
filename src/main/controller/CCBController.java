d1d0ef2e1ac9b8313c202ba438f7dce933ac2638
adrián jaén <adrianjaensiu@gmail.com>
2025-07-16 16:36:03 -0400

    Create CCBController.java

---
+++ src/main/controller/CCBController.java
@@ -0,0 +1,32 @@
+package src.main.controller;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import src.main.utils.Navigate;
+import src.main.view.pages.CCBView;
+
+public class CCBController implements ActionListener {
+
+    private CCBView view;
+
+    public CCBController(CCBView view) {
+        this.view = view;
+        this.view.setController(this);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        String command = e.getActionCommand();
+        
+        if ("CALCULAR".equals(command)) {
+           
+            System.out.println("Costos Fijos: " + view.getCostosFijos());
+System.out.println("Costos Variables: " + view.getCostosVariables());
+System.out.println("Número de bandejas: " + view.getNumeroBandejas()); 
+System.out.println("Merma: " + view.getPorcentajeMerma());
+        } 
+        else if ("VOLVER".equals(command)) {
+            view.dispose();
+            Navigate.getInstance().initPrincipalView();
+        }
+    }
+}
