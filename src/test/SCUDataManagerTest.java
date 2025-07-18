20bd21523a9537a5f4f2f6031b9f9a19682e3879
Isaac <isaacpereztouceda@gmail.com>
2025-07-18 00:21:43 -0400

    Deleting more stuff

--- src/test/SCUDataManagerTest.java
+++ src/test/SCUDataManagerTest.java
@@ -2,7 +2,6 @@ package src.test;
 import src.main.model.SCUDataManager;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 import java.io.File;
