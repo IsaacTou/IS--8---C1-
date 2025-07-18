4f0332c188d68bfe134f6e7a2db9dcd9715075b3
Santiago <150873551+santiago-commit@users.noreply.github.com>
2025-07-18 00:32:32 +0000

    Update WelcomeController.java

--- src/main/controller/WelcomeController.java
+++ src/main/controller/WelcomeController.java
@@ -1,5 +1,4 @@
 package src.main.controller;
-
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import src.main.utils.Navigate;
@@ -7,25 +6,28 @@ import src.main.view.pages.WelcomeView;
 
 public class WelcomeController implements ActionListener {
 
-    private WelcomeView view;
+	private WelcomeView view;
 
-    public WelcomeController(WelcomeView view) {
-        this.view = view;
-        view.setController(this);
-        view.setVisible(true);
-    }
+	public WelcomeController(WelcomeView view) {
+		this.view = view;
+		view.setVisible(true);
+	}
 
-    @Override
-    public void actionPerformed(ActionEvent e) {
-        switch (e.getActionCommand()) {
-            case "GO_TO_LOGIN":
-                view.dispose();
-                Navigate.getInstance().initLoginView();
-                break;
-            case "GO_TO_REGISTER":
-                view.dispose();
-                Navigate.getInstance().initRegisterView();
-                break;
-        }
-    }
+	public void initialize() {
+		view.setController(this);
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		switch (e.getActionCommand()) {
+			case "GO_TO_LOGIN":
+			view.dispose();
+			Navigate.getInstance().initLoginView();
+			break;
+			case "GO_TO_REGISTER":
+			view.dispose();
+			Navigate.getInstance().initRegisterView();
+			break;
+		}
+	}
 }
