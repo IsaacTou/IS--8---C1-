486bd890b9b3e4b4eb45df1aff5ee715e3c7dfd8
Santiago <150873551+santiago-commit@users.noreply.github.com>
2025-07-18 00:33:13 +0000

    Update Navigate.java

--- src/main/utils/Navigate.java
+++ src/main/utils/Navigate.java
@@ -3,64 +3,63 @@ import src.main.controller.*;
 import src.main.view.pages.*;
 
 public class Navigate {
-   
-    private static Navigate instance;
 
-    private Navigate() {} 
+	private static Navigate instance;
 
-    public static Navigate getInstance() {
-        if (instance == null) {
-            instance = new Navigate();
-        }
-        return instance;
-    }
+	private Navigate() {} 
 
-    public void initRegister() {
-        RegisterView register = new RegisterView();
-        new RegisterController(register);
-        register.setVisible(true);     
-    }
+	public static Navigate getInstance() {
+		if (instance == null) {
+			instance = new Navigate();
+		}
+		return instance;
+	}
 
-    public void initLogin() {
-        LoginView login = new LoginView();
-        new LoginController(login);
-        login.setVisible(true);
-    }
-    
-    public void initPurse() {
-        PurseView purse = new PurseView();
-        new PurseController(purse);
-        purse.setVisible(true);
-    }
+	public void initRegister() {
+		RegisterView register = new RegisterView();
+		new RegisterController(register).initialize();
+		register.setVisible(true);     
+	}
 
-    public void initPrincipal() {
-        PrincipalView view = new PrincipalView();
-        new PrincipalController(view);
-        view.setVisible(true);
-    }
+	public void initLogin() {
+		LoginView login = new LoginView();
+		new LoginController(login).initialize();
+		login.setVisible(true);
+	}
 
-    public void initCCBView() {
-        CCBView ccbView = new CCBView();
-        new CCBController(ccbView);
-        ccbView.setVisible(true);
-    }
+	public void initPurse() {
+		PurseView purse = new PurseView();
+		new PurseController(purse).initialize();
+		purse.setVisible(true);
+	}
 
-    public void initLoginView() {
-        LoginView login = new LoginView();
-        new LoginController(login);  
-        login.setVisible(true); 
-    }
-    
-    public void initRegisterView() {
-        RegisterView register = new RegisterView();
-        new RegisterController(register);  
-        register.setVisible(true);  
-    }
-    
-    public void initWelcomeView() {
-        WelcomeView welcomeView = new WelcomeView();
-        new WelcomeController(welcomeView);
-        welcomeView.setVisible(true);       
-    }
-    
+	public void initPrincipal() {
+		PrincipalView view = new PrincipalView();
+		new PrincipalController(view).initialize();
+		view.setVisible(true);
+	}
+
+	public void initCCBView() {
+		CCBView ccbView = new CCBView();
+		new CCBController(ccbView).initialize();
+		ccbView.setVisible(true);
+	}
+
+	public void initLoginView() {
+		LoginView login = new LoginView();
+		new LoginController(login).initialize(); 
+		login.setVisible(true); 
+	}
+
+	public void initRegisterView() {
+		RegisterView register = new RegisterView();
+		new RegisterController(register).initialize();
+		register.setVisible(true);  
+	}
+
+	public void initWelcomeView() {
+		WelcomeView welcomeView = new WelcomeView();
+		new WelcomeController(welcomeView).initialize();
+		welcomeView.setVisible(true);       
+	}
 }
