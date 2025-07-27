a349ecc08ac2dbd54ad0f10591ee4b4b229b122f
Santiago <150873551+santiago-commit@users.noreply.github.com>
2025-07-27 00:42:13 +0000

    Add files via upload

--- src/main/model/SesionUser.java
+++ src/main/model/SesionUser.java
@@ -1,28 +1,55 @@
 package src.main.model;
+import java.io.*;
+import java.nio.file.*;
+import java.util.*;
 
 public class SesionUser {
-    private static SesionUser instance;
-    private User sesionUser;
+	private static SesionUser instance;
+	private User sesionUser;
 
-    private SesionUser() {} 
+	private SesionUser() {} 
 
-    public static SesionUser getInstance() {
-        if (instance == null) {
-            instance = new SesionUser();
-        }
-        return instance;
-    }
+	public static SesionUser getInstance() {
+		if (instance == null) {
+			instance = new SesionUser();
+		}
+		return instance;
+	}
 
-    public void setSesionUser(User user) {
-        this.sesionUser = user;
-    }
+	public void setSesionUser(User user) {
+		this.sesionUser = user;
+	}
 
-    public User getUser() {
-        return sesionUser;
-    }
+	public User getUser() {
+		return sesionUser;
+	}
 
-    public void logout() {
-        this.sesionUser = null;
-    }
+	public void logout() {
+		this.sesionUser = null;
+	}
 
+	public int setWallet(String amount) {
+		try {
+			String filename = "src/main/data/SCUDataBase.txt";
+			List<String> lines = Files.readAllLines(Paths.get(filename));
+
+			for (int i = 0; i < lines.size(); i++) {
+				String line = lines.get(i);
+				if (line.startsWith(instance.getUser().getCi())) {
+					String[] parts = line.split(",", -1);
+					parts[parts.length - 1] = Float.toString(
+						Float.parseFloat(parts[parts.length - 1])
+						+ Float.parseFloat(amount)
+					);
+					lines.set(i, String.join(",", parts));
+					Files.write(Paths.get(filename), lines);
+					return 0;
+				}
+			}
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return 1;
+	}
 }
