fc61ec93d16c01d66ef6b036030875ea7572b570
Santiago <150873551+santiago-commit@users.noreply.github.com>
2025-07-27 15:32:21 +0000

    Add files via upload

--- src/main/model/User.java
+++ src/main/model/User.java
@@ -1,29 +1,49 @@
 package src.main.model;
+import java.io.*;
+import java.nio.file.*;
+import java.util.*;
 
 public class User {
-    private String ci;
-    private String user;
-    private String userType;
-    private String wallet;
+	private String ci;
+	private String user;
+	private String userType;
+	private String wallet;
 
-    public User(String ci, String user, String userType, String wallet) {
-        this.ci = ci;
-        this.user = user;
-        this.userType = userType;
-        this.wallet = wallet;
-    }
+	public User(String ci, String user, String userType, String wallet) {
+		this.ci = ci;
+		this.user = user;
+		this.userType = userType;
+		this.wallet = wallet;
+	}
 
-    public String getCi () {
-        return this.ci;
-    }
-    public String getUser () {
-        return this.user;
-    }
-    public String getUserType () {
-        return this.userType;
-    }
-    public String getWallet () {
-        return this.wallet;
-    }
+	public String getCi() {
+		return this.ci;
+	}
 
+	public String getUser() {
+		return this.user;
+	}
+
+	public String getUserType() {
+		return this.userType;
+	}
+
+	public String getWallet() {
+		try {
+			String filename = "src/main/data/SCUDataBase.txt";
+			List<String> lines = Files.readAllLines(Paths.get(filename));
+
+			for (int i = 0; i < lines.size(); i++) {
+				String line = lines.get(i);
+				if (line.startsWith(this.getCi())) {
+					String[] parts = line.split(",", -1);
+					return parts[parts.length - 1];
+				}
+			}
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return this.wallet;
+	}
 }
