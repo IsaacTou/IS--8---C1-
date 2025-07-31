283264c7ea73ab48a21f96069253ee9d57bf6b15
Santiago <150873551+santiago-commit@users.noreply.github.com>
2025-07-31 00:52:02 +0000

    Add files via upload

--- src/main/view/pages/PrincipalView.java
+++ src/main/view/pages/PrincipalView.java
@@ -1,5 +1,5 @@
 package src.main.view.pages;
-    
+
 import java.awt.Color;
 import java.awt.Font;
 import java.awt.event.ActionListener;
@@ -9,16 +9,17 @@ import src.main.view.components.*;
 
 @SuppressWarnings("serial")
 public class PrincipalView extends JFrame {
-    
+
 	JPanel panel;
-    JPanel separationPanel;
-    RoundedPanel centerPanel;
+	JPanel separationPanel;
+	// RoundedPanel centerPanel;
 	ImagePanel imageBackground;
-    ImagePanel centerBackground;
-    ConsumeButton consume;
-    CheckMenuButton checkMenu;
-    CheckPurseButton checkPurse;
-    LogoutButton logout;
+	ImagePanel centerBackground;
+	ConsumeButton consume;
+	CheckMenuButton checkMenu;
+	CheckPurseButton checkPurse;
+	LogoutButton logout;
+	JLabel walletLabel;
 
 	int boxXPosition = 520;
 	int ciBoxYPosition = 230;
@@ -26,112 +27,122 @@ public class PrincipalView extends JFrame {
 	int keyBoxYPosition = 330;
 	int xPosition = 440;
 
-
 	public PrincipalView() {
-    setSize(850,650);
-    setDefaultCloseOperation(EXIT_ON_CLOSE);
-    setTitle("SGCU"); 
-    setLocationRelativeTo(null);
-    addPanel();
-    addSeparationPanel();
-    addCenterPanel();
-    addButtons();
-    addImage();
+		setSize(850,650);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		setTitle("SGCU"); 
+		setLocationRelativeTo(null);
+		addPanel();
+		addSeparationPanel();
+		// addCenterPanel();
+		addButtons();
+		addImage();
+	}
 
-    }
+	public void isAdmin () { addAdminButtons(); }
 
-    public void isAdmin () { addAdminButtons(); }
+	public void userInfo(String user, String wallet) {
+		JLabel title = new JLabel("¡Bienvenido/a " + user + "!");
+		title.setBounds(0, 10, 220, 30); 
+		title.setHorizontalAlignment(SwingConstants.CENTER);
+		title.setFont(new Font("Sans Serif", Font.PLAIN, 20));
+		title.setForeground(new Color(0, 51, 102)); 
+		walletLabel = new JLabel("Su saldo disponible es de: " + wallet);
+		walletLabel.setBounds(0, 30, 220, 30); 
+		walletLabel.setHorizontalAlignment(SwingConstants.CENTER);
+		walletLabel.setFont(new Font("Sans Serif", Font.PLAIN, 15));
+		walletLabel.setForeground(new Color(0, 51, 102));
+		panel.add(title); 
+		panel.add(walletLabel);
+	}
 
-    public void userInfo(String user, String wallet) {
-        JLabel title = new JLabel("¡Bienvenido/a " + user + "!");
-        title.setBounds(0, 10, 220, 30); 
-        title.setHorizontalAlignment(SwingConstants.CENTER);
-        title.setFont(new Font("Sans Serif", Font.PLAIN, 20));
-        title.setForeground(new Color(0, 51, 102)); 
-        JLabel Wallet = new JLabel("Su saldo disponible es de: " + wallet);
-        Wallet.setBounds(0, 30, 220, 30); 
-        Wallet.setHorizontalAlignment(SwingConstants.CENTER);
-        Wallet.setFont(new Font("Sans Serif", Font.PLAIN, 15));
-        Wallet.setForeground(new Color(0, 51, 102));
-        panel.add(title); 
-        panel.add(Wallet);
-    }
+	public void refreshBalance(String wallet) {
+		walletLabel.setText("Su saldo disponible es de: " + wallet);
+	}
 
 	private void addPanel() {
-        panel = new JPanel();
-        panel.setLayout(null);
-        panel.setBounds(0, 0, 220, 650); // Panel izquierdo
-        this.getContentPane().add(panel);
-    }
+		panel = new JPanel();
+		panel.setLayout(null);
+		panel.setBounds(0, 0, 220, 650); // Panel izquierdo
+		this.getContentPane().add(panel);
+	}
 
-    private void addSeparationPanel() {
-        separationPanel = new JPanel();
-        separationPanel.setLayout(null);
-        separationPanel.setBounds(0, 200, 220, 50); 
-        separationPanel.setBackground(new Color(40, 55, 71 ));
-        JLabel title = new JLabel("APARTADOS");
-        title.setBounds(0, 10, 220, 30); 
-        title.setHorizontalAlignment(SwingConstants.CENTER);
-        title.setFont(new Font("Sans Serif", Font.BOLD, 20));
-        title.setForeground(Color.WHITE); 
-        separationPanel.add(title); 
-        
-        panel.add(separationPanel);
-    }
-    
-    private void addImage() {
-        imageBackground = new ImagePanel("principal_background.png",850, 650, 230, 0);
-        this.getContentPane().add(imageBackground);
-    }
+	private void addSeparationPanel() {
+		separationPanel = new JPanel();
+		separationPanel.setLayout(null);
+		separationPanel.setBounds(0, 200, 220, 50); 
+		separationPanel.setBackground(new Color(40, 55, 71 ));
+		JLabel title = new JLabel("APARTADOS");
+		title.setBounds(0, 10, 220, 30); 
+		title.setHorizontalAlignment(SwingConstants.CENTER);
+		title.setFont(new Font("Sans Serif", Font.BOLD, 20));
+		title.setForeground(Color.WHITE); 
+		separationPanel.add(title); 
+		panel.add(separationPanel);
+	}
 
-    private void addCenterPanel() {
-        centerPanel = new RoundedPanel(30); 
-        centerPanel.setBackground(Color.WHITE); 
-        centerPanel.setLayout(null);
-        centerPanel.setBounds(420, 200, 200, 250);
-        centerBackground = new ImagePanel("user_icon.png",100,100,50,50);
-        centerPanel.add(centerBackground);
+	private void addImage() {
+		imageBackground = new ImagePanel("principal_background.png",850, 650, 230, 0);
+		this.getContentPane().add(imageBackground);
+	}
 
-        this.getContentPane().add(centerPanel);
-    }
+	/*
+	private void addCenterPanel() {
+		centerPanel = new RoundedPanel(30); 
+		centerPanel.setBackground(Color.WHITE); 
+		centerPanel.setLayout(null);
+		centerPanel.setBounds(420, 200, 200, 250);
+		centerBackground = new ImagePanel("user_icon.png",100,100,50,50);
+		centerPanel.add(centerBackground);
 
-    private void addButtons() {
-        consume = new ConsumeButton();
-        consume.setBounds(40, 170, 120, 40);
-        consume.setFont(new Font("Arial", Font.BOLD, 14));
-        centerPanel.add(consume);
-    
-        checkPurse = new CheckPurseButton(); 
-        checkPurse.setBounds(40, 60, 130, 25);
-        checkPurse.setFont(new Font("Arial", Font.BOLD, 18));
-        panel.add(checkPurse);
-    
-        checkMenu = new CheckMenuButton();
-        checkMenu.setBounds(18, 300, 190, 50);
-        checkMenu.setActionCommand("CONSULTAR_MENUS"); // Asegurar este comando
-        panel.add(checkMenu);
+		this.getContentPane().add(centerPanel);
+	}
+    */
 
-        logout = new LogoutButton();
-        logout.setBounds(30, 110, 160, 50);
-        panel.add(logout);
+	private void addButtons() {
+		/*
+		consume = new ConsumeButton();
+		consume.setBounds(40, 170, 120, 40);
+		consume.setFont(new Font("Arial", Font.BOLD, 14));
+		centerPanel.add(consume);
+		*/
 
-    }
+		checkPurse = new CheckPurseButton(); 
+		checkPurse.setBounds(40, 60, 130, 25);
+		checkPurse.setFont(new Font("Arial", Font.BOLD, 18));
+		panel.add(checkPurse);
 
-    public void setController(ActionListener controller) {
-        checkPurse.addActionListener(controller);
-        checkMenu.addActionListener(controller);
-        logout.addActionListener(controller);
-    }
+		checkMenu = new CheckMenuButton();
+		checkMenu.setBounds(18, 300, 190, 50);
+		checkMenu.setActionCommand("CONSULTAR_MENUS"); // Asegurar este comando
+		panel.add(checkMenu);
 
-    private void addAdminButtons() {
-    CalculateCCBButton calculateCCBButton = new CalculateCCBButton();
-    calculateCCBButton.setBounds(18, 350, 190, 50);
-    calculateCCBButton.addActionListener(e -> {
-        this.dispose();
-        Navigate.getInstance().initCCBView();
-    });
-    panel.add(calculateCCBButton);
+		logout = new LogoutButton();
+		logout.setBounds(30, 110, 160, 50);
+		panel.add(logout);
+	}
+
+	public void setController(ActionListener controller) {
+		checkPurse.addActionListener(controller);
+		checkMenu.addActionListener(controller);
+		logout.addActionListener(controller);
+	}
+
+	private void addAdminButtons() {
+		CalculateCCBButton calculateCCBButton = new CalculateCCBButton();
+		calculateCCBButton.setBounds(18, 350, 190, 50);
+		calculateCCBButton.addActionListener(e -> {
+			this.dispose();
+			Navigate.getInstance().initCCBView();
+		});
+
+		ScanMenuButton scanMenuButton = new ScanMenuButton();
+		scanMenuButton.setBounds(18, 350 + 50, 190, 50);
+		scanMenuButton.addActionListener(e -> {
+			this.dispose();
+			Navigate.getInstance().initScanMenuView();
+		});
+		panel.add(calculateCCBButton);
+		panel.add(scanMenuButton);
+	}
 }
-
-}
-
