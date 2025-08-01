f9153edc18aeceff3398e7f525b916e8e16630f7
Santiago <150873551+santiago-commit@users.noreply.github.com>
2025-08-01 03:32:53 +0000

    Update ScannerDataTest.java

--- src/test/ScannerDataTest.java
+++ src/test/ScannerDataTest.java
@@ -14,37 +14,34 @@ public class ScannerDataTest {
 
 	File invalid, valid;
 
-	@BeforeEach
-	public void setUp() {
-		invalid = new File("invalid_route");
-		valid = new File("src/main/data/FaceID/test.jpg");
-		
-	}
-
 	@Test
 	public void testIsEqualInvalidFiles() {
-		assertEquals(
-			ScannerData.isEqual(invalid, valid),
-			false,
-			"Los archivos son inválidos. Este test debería retornar false."
-		);
+		invalid = new File("invalid");
+		valid = new File("src/main/data/FaceID/test.jpg");
 		assertEquals(
 			ScannerData.isEqual(valid, invalid),
 			false,
-			"El primer archivo es inválido. Este test debería retornar false."
+			"El segundo archivo es inválido. Este test debería retornar false."
 		);
 		assertEquals(
 			ScannerData.isEqual(invalid, invalid),
 			false,
-			"El segundo archivo es inválido. Este test debería retornar false."
+			"Los archivos son inválidos. Este test debería retornar false."
+		);
+		assertEquals(
+			ScannerData.isEqual(invalid, valid),
+			false,
+			"El primer archivo es inválido. Este test debería retornar false."
 		);
 	}
 
 	@Test
 	public void testIsEqual() {
+		valid = new File("src/main/data/FaceID/test.jpg");
 		assertTrue(
 			ScannerData.isEqual(valid, valid),
 			"Una imagen debería ser igual a sí misma."
 		);
 	}
 }
+
